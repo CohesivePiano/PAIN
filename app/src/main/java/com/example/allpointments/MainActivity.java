2f95f31e5d7586ec610c24bd5dbf33be16fc147b
Colton Peverley <coltonpev@gmail.com>
2021-02-20 18:31:48 -0800

    Major Change

--- app/src/main/java/com/example/allpointments/MainActivity.java
+++ app/src/main/java/com/example/allpointments/MainActivity.java
@@ -19,18 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
